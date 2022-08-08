d1eab7b4127bf5d5bf2fa08dc5c800fc6e2cc1e5
Rizal Burhanudin <rizalburhanudin556@gmail.com>
2022-08-08 15:53:47 +0700

    "Belajar modbus TCP/IP"

---
+++ modbus_slave/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace modbus_slave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTcp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.s = new System.Windows.Forms.Label();
+            this.txtRegister = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtWriteRegister = new System.Windows.Forms.TextBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSlave = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TCP/IP";
+            // 
+            // txtTcp
+            // 
+            this.txtTcp.Location = new System.Drawing.Point(32, 59);
+            this.txtTcp.Name = "txtTcp";
+            this.txtTcp.Size = new System.Drawing.Size(170, 20);
+            this.txtTcp.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(32, 103);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(170, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PORT";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(32, 129);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 42);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(113, 129);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(89, 42);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "DISCONNECT";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(32, 177);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(170, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status :";
+            // 
+            // s
+            // 
+            this.s.AutoSize = true;
+            this.s.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s.Location = new System.Drawing.Point(29, 228);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(51, 30);
+            this.s.TabIndex = 8;
+            this.s.Text = "OFF";
+            // 
+            // txtRegister
+            // 
+            this.txtRegister.Location = new System.Drawing.Point(236, 59);
+            this.txtRegister.Name = "txtRegister";
+            this.txtRegister.Size = new System.Drawing.Size(196, 20);
+            this.txtRegister.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Registers:";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(441, 54);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(56, 29);
+            this.btnRead.TabIndex = 11;
+            this.btnRead.Text = "READ";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtWriteRegister
+            // 
+            this.txtWriteRegister.Location = new System.Drawing.Point(308, 103);
+            this.txtWriteRegister.Name = "txtWriteRegister";
+            this.txtWriteRegister.Size = new System.Drawing.Size(73, 20);
+            this.txtWriteRegister.TabIndex = 12;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(387, 103);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(74, 20);
+            this.txtValue.TabIndex = 13;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(467, 98);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(87, 29);
+            this.btnWrite.TabIndex = 14;
+            this.btnWrite.Text = "WRITE";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(305, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Registers";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(384, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Value";
+            // 
+            // txtSlave
+            // 
+            this.txtSlave.Location = new System.Drawing.Point(236, 103);
+            this.txtSlave.Name = "txtSlave";
+            this.txtSlave.Size = new System.Drawing.Size(66, 20);
+            this.txtSlave.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(233, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Slave";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(236, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(318, 136);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(503, 54);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(56, 29);
+            this.btnStop.TabIndex = 20;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 277);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtSlave);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.txtWriteRegister);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRegister);
+            this.Controls.Add(this.s);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTcp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTcp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label s;
+        private System.Windows.Forms.TextBox txtRegister;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtWriteRegister;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSlave;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ modbus_slave/Form1.cs
@@ -0,0 +1,143 @@
+﻿using Modbus.Device;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modbus_slave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDisconnect.Enabled = false;
+            txtRegister.Enabled = false;
+            txtValue.Enabled = false;
+            btnRead.Enabled = false;
+            txtSlave.Enabled = false;
+            txtWriteRegister.Enabled = false;
+            btnWrite.Enabled = false;
+            btnStop.Enabled = false;
+        }
+        TcpClient client;
+        ModbusIpMaster master;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (txtTcp.Text != "" && txtPort.Text != "")
+            {
+                client = new TcpClient();
+                client.BeginConnect(txtTcp.Text, int.Parse(txtPort.Text), null, null);
+                master = ModbusIpMaster.CreateIp(client);
+                txtTcp.Enabled = false;
+                txtPort.Enabled = false;
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                txtRegister.Enabled = true;
+                txtValue.Enabled = true;
+                btnRead.Enabled = true;
+                txtSlave.Enabled = true;
+                txtWriteRegister.Enabled = true;
+                btnWrite.Enabled = true;
+                btnStop.Enabled = true;
+                progressBar1.Value = 100;
+                s.Text = "ON";
+            }
+            else
+            {
+                MessageBox.Show("Lengkapi semua data");
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            client.Close();
+            txtTcp.Enabled = true;
+            txtPort.Enabled = true;
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            txtRegister.Enabled = false;
+            txtValue.Enabled = false;
+            btnRead.Enabled = false;
+            txtSlave.Enabled = false;
+            txtWriteRegister.Enabled = false;
+            btnWrite.Enabled = false;
+            btnStop.Enabled = false;
+            progressBar1.Value = 0;
+            s.Text = "OFF";
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (btnDisconnect.Enabled == true)
+            {
+                timer1.Stop();
+            }
+            try
+            {
+                if (txtRegister.Text != "")
+                {
+                    ushort startRef = Convert.ToUInt16(txtRegister.Text);
+                    ushort noOfRef = 1;
+
+                    ushort[] outputRegisterValue = master.ReadHoldingRegisters(1, startRef, noOfRef);
+                    List<EntitySleve> list = new List<EntitySleve>();
+                    for (int i = 0; i < outputRegisterValue.Length; i++)
+                    {
+                        list.Add(new EntitySleve
+                        {
+                            Registers = startRef.ToString(),
+                            Value = outputRegisterValue[i].ToString()
+                        });
+                    }
+                    BindingSource bs = new BindingSource();
+                    bs.DataSource = list;
+                    dataGridView1.DataSource = bs;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (btnDisconnect.Enabled == true)
+            {
+                timer1.Start();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte slaveAddress = Convert.ToByte(txtSlave.Text);
+                ushort startRef = Convert.ToUInt16(txtWriteRegister.Text);
+                ushort value = Convert.ToUInt16(txtValue.Text);
+                master.WriteSingleRegister(slaveAddress, startRef, value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
